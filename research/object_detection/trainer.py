2e7de7efa68dc2ed50d8c9407e43d2a9c0283bd4
dek <dakoner@gmail.com>
2018-05-25 11:26:43 -0700

    disable soft placement and enable device placement logging.

--- research/object_detection/trainer.py
+++ research/object_detection/trainer.py
@@ -352,8 +352,8 @@ def train(create_tensor_dict_fn,
     summary_op = tf.summary.merge(list(summaries), name='summary_op')
 
     # Soft placement allows placing on CPU ops without GPU implementation.
-    session_config = tf.ConfigProto(allow_soft_placement=True,
-                                    log_device_placement=False)
+    session_config = tf.ConfigProto(allow_soft_placement=False,
+                                    log_device_placement=True)
 
     # Save checkpoints regularly.
     keep_checkpoint_every_n_hours = train_config.keep_checkpoint_every_n_hours
